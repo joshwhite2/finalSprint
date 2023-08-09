0fcb6b36bc6e9f87c40584d234b45167eab9453c
Ryan Crowley <ryan.g.crowley@gmail.com>
2023-08-08 22:23:01 -0230

    aug8night

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ShoppingCart from "./ShoppingCart";
 
 const Header = () => {
   return (
     <div className="header">
       <Link to="/">Products</Link>
-      <Link to="/cart">View Cart</Link>
+      <Link to="/cart">
+        <ShoppingCart />
+      </Link>
     </div>
   );
 };
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import BackButton from "../shared/BackButton";
+import { Link, useLocation } from "react-router-dom";
+import FormattedPrice from "./FormattedPrice";
+import { getProducts } from "../data/api";
 
+// making a drop down preview of what is already in cart
 const ShoppingCart = () => {
+  const cartItems = getProducts();
+  const [isPreviewOpen, setIsPreviewOpen] = useState(false);
+
+  const togglePreview = () => {
+    setIsPreviewOpen(!isPreviewOpen);
+  };
+
+  // using useLocation to remove dropdown cart from cart page
+  const location = useLocation();
+  const isCartPage = location.pathname === "/cart";
+
   return (
     <>
-      <div>ShoppingCart</div>
-      <BackButton></BackButton>
+      {!isCartPage && (
+        <div
+          className="cart-icon"
+          onMouseEnter={togglePreview}
+          onMouseLeave={togglePreview}
+        >
+          <Link to="/cart">
+            <img src="/images/cart-icon.png" alt="view cart" />
+          </Link>
+          {isPreviewOpen && (
+            <div className="cart-preview">
+              {cartItems.length > 0 ? (
+                cartItems.map((item) => (
+                  <div key={item.id}>
+                    <p>
+                      {item.name} - <FormattedPrice value={item.price} />
+                    </p>
+                  </div>
+                ))
+              ) : (
+                <p> Your cart is empty</p>
+              )}
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 };
