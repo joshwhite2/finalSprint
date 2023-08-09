c800da6524bb0297939491506ec746dd8c9fb5e8
Josh White <josh.white@keyin.com>
2023-08-09 20:08:45 -0230

    addtocart work

--- src/context/ShoppingCartContext.js
+++ src/context/ShoppingCartContext.js
@@ -8,20 +8,16 @@ export const ShoppingCartProvider = ({ children }) => {
   const fetchShoppingCartData = async () => {
     try {
       const res = await fetch("http://localhost:5000/shoppingcart");
-
       const data = await res.json();
-
       return data;
     } catch (error) {
       console.error("Error fetching shopping cart data:", error);
-
       return [];
     }
   };
 
   const fetchShoppingCart = async () => {
     const shoppingCartFromServer = await fetchShoppingCartData();
-
     setShoppingCart(shoppingCartFromServer);
   };
 
@@ -29,33 +25,42 @@ export const ShoppingCartProvider = ({ children }) => {
     fetchShoppingCart(); // Fetch and populate the shopping cart initially
   }, []);
 
-  const addToCart = async (newItem) => {
+  const addToCart = async (item) => {
     try {
-      const response = await fetch("http://localhost:5000/shoppingcart", {
-        method: "POST",
-
-        headers: {
-          "Content-Type": "application/json",
-        },
-
-        body: JSON.stringify(newItem),
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
+      const existingCartItem = shoppingCart.find((cartItem) => cartItem.id === item.id);
+  
+      if (existingCartItem) {
+        // If item already exists in cart, update its quantity
+        const updatedCart = shoppingCart.map((cartItem) =>
+          cartItem.id === item.id ? { ...cartItem, quantity: cartItem.quantity +1 } : cartItem
+        );
+        setShoppingCart(updatedCart);
+      } else {
+        // If item doesn't exist in cart, add it
+        const response = await fetch("http://localhost:5000/shoppingcart", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(item),
+        });
+  
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+  
+        // If the add request was successful, update the shopping cart state
+        setShoppingCart((prevCart) => [...prevCart, {...item, quantity:1}]);
       }
-
-      setShoppingCart((prevCart) => [...prevCart, newItem]);
     } catch (error) {
       console.error("Error adding item to cart:", error);
     }
   };
-
   return (
     <ShoppingCartContext.Provider
       value={{
         shoppingCart,
-
+        setShoppingCart,
         addToCart,
       }}
     >
